5e2c9099bbc708f81796f6ba65255897204c1464
Okpalaeze patrick Echezona <129857369+Echez-cpu@users.noreply.github.com>
2025-01-08 22:19:10 +0100

    Update main.cpp

--- ex03/main.cpp
+++ ex03/main.cpp
@@ -4,10 +4,10 @@
 bool bsp( Point const a, Point const b, Point const c, Point const point);
 
 int	main() {
-	Point	a(2.02f, 0.83f);
-	Point	b(2.66f, 1.68f);
-	Point	c(1.24f, 1.91f);
-	Point	p(2.07f, 1.46f);
+	Point	a(0.0f, 0.0f);
+	 Point	b(10.0f, 30.0f);
+	 Point	c(20.0f, 0.0f);
+	 Point	p(10.0f, 0.1f);
 
 	if (bsp(a, b, c, p) == true)
 		 std::cout << "\033[32mThe point is inside the triangle.\033[0m" << std::endl;  // zero means it is on the line
@@ -15,3 +15,9 @@ int	main() {
 		std::cout << "\033[31mThe point is a vertex or on the edge or outside the traingle.\033[0m" << std::endl;
 	return 0;
 }
+
+
+           Point	a(0.0f, 0.0f);
+	// Point	b(10.0f, 30.0f);
+	// Point	c(20.0f, 0.0f);
+	// Point	p(20.0f, 0.1f);
