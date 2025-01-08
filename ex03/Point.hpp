035c75fc305bcb96ca8a3f02e4675e12239bafdd
Okpalaeze patrick Echezona <129857369+Echez-cpu@users.noreply.github.com>
2025-01-08 12:56:27 +0100

    Update Point.hpp

--- ex03/Point.hpp
+++ ex03/Point.hpp
@@ -1,1 +1,27 @@
+#ifndef POINT_HPP
+#define POINT_HPP
 
+#include "Fixed.hpp"
+#include <iostream>
+
+class Point {
+	public:
+		Point();
+		Point(float const x, float const y);
+		Point(Point const &source);
+		~Point();
+
+		Point &	operator=(Point const &original_copy);
+		Point	operator-(Point const &rhSide);
+		
+		Fixed const	getX() const;
+		Fixed const	getY() const;
+		
+	private:
+		Fixed const	x;
+		Fixed const	y;
+};
+
+std::ostream&	operator<<(std::ostream &output, Point const &pointObj);
+
+#endif
